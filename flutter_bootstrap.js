643c2d5cff127b8f2d7799fe01bc9680917091b1
HFCristian <cristianhassfretes@gmail.com>
2025-10-20 06:52:44 -0400

    20/10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1320153616"
+    serviceWorkerVersion: "1331312295"
   }
 });
